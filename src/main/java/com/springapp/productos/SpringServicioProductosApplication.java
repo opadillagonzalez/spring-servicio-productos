f03583a52b4e9ec1b37fe7b2fd0a97516191543d
Oscar Padilla <osckarpgonzalez@gmail.com>
2022-10-15 17:25:07 -0400

    Primer commit servicio productos

---
+++ src/main/java/com/springapp/productos/SpringServicioProductosApplication.java
@@ -0,0 +1,13 @@
+package com.springapp.productos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringServicioProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringServicioProductosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springapp/productos/SpringServicioProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springapp.productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringServicioProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
